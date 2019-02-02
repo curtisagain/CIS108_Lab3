8a69ae07e21ff86602acee05eb233c571ab0ecd0
curtisagain <46700844+curtisagain@users.noreply.github.com>
2019-02-01 21:59:13 -0800

    Update Calculator.h

--- CIS108_Lab3/Exercise1/Calculator.h
+++ CIS108_Lab3/Exercise1/Calculator.h
@@ -3,8 +3,13 @@
 #include <conio.h>
 namespace Calculate
 {
-	double add(double, double);
-	double subtract(double, double);
-	double multiply(double, double);
-	double divide(double, double);
+	double add(double num1, double num2);
+	double subtract(double num1, double num2);
+	double multiply(double num1, double num2);
+	double divide(double num1, double num2);
+	double power(double num1, double num2);
+	void save(double input_number);
+	double recall(); //these two functions don't need outputs.
+	void memClear();
+	double invertSign(double input_number);
 }
